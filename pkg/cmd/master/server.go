295b795b187b590d073051122d4e6a34b015c1ac
Jeff Cantrill <jcantril@redhat.com>
2014-10-02 10:54:18 -0400

    ping server to confirm it starts

--- pkg/cmd/master/server.go
+++ pkg/cmd/master/server.go
@@ -1,12 +1,15 @@
 package master
 
 import(
+	"net"
+	"time"
 	kubeclient 	"github.com/GoogleCloudPlatform/kubernetes/pkg/client"
 	osclient 	"github.com/openshift/origin/pkg/client"
 )
 
 type ServerConfig struct {
   config
+  Timeout time.Duration
 }
 
 type MasterServer struct{
@@ -33,15 +36,42 @@ func (s *MasterServer) KubeClient() *kubeclient.Client {
 	return s.getKubeClient()
 }
 
-func (cfg *ServerConfig) StartAllInOne() *MasterServer {
+func (cfg *ServerConfig) StartAllInOne() (*MasterServer, error) {
 	done := make(chan int, 1)
 	go func(){
 		cfg.startAllInOne(done)
 	}()
+	
+	
+	unblock := make(chan int, 1)
+	var err error
+	
+	go func(){
+		start := time.Now()
+		for elapsed := 0 * time.Nanosecond; cfg.Timeout > elapsed; elapsed = time.Now().Sub(start){
+			dialer := net.Dialer{
+				Timeout: cfg.Timeout, 
+				DualStack: true,
+			}
+			var conn net.Conn
+			conn, err = dialer.Dial("tcp", cfg.ListenAddr)
+			if conn != nil{
+				unblock <- 1
+	  			defer conn.Close()
+				break
+			}
+		}
+	}()
+	//block until server is up
+	<-unblock
+	if err != nil{
+		return nil, err
+	}
+
 	return &MasterServer{
 		sig: done,
 		ServerConfig: *cfg,
-	}
+	}, nil
 }
 
 func (s *MasterServer) Stop(){
--- test/integration/endToEnd_test.go
+++ test/integration/endToEnd_test.go
@@ -2,40 +2,45 @@ package integration
 
 import (
 	"testing"
+	"time"
 //	"encoding/json"
 //	kubeclient "github.com/GoogleCloudPlatform/kubernetes/pkg/client"
 	//the required functionality moved to an 'api' package?
 //	kubeopts "github.com/GoogleCloudPlatform/kubernetes/pkg/client"
 //	osclient "github.com/openshift/origin/pkg/client"
-//	buildapi "github.com/openshift/origin/pkg/build/api"
+	buildapi "github.com/openshift/origin/pkg/build/api"
 			 "github.com/openshift/origin/pkg/cmd/master"
 )
 
 func TestEndToEnd(t *testing.T){
 	//create server
 	serverConfig := master.NewServerConfig("127.0.0.1:8080", "127.0.0.1", []string{"127.0.0.1"})
-	server := serverConfig.StartAllInOne()
+	serverConfig.Timeout = 10 * time.Second
+	server, err := serverConfig.StartAllInOne()
+	if err != nil{
+		t.Error("Unable to start the server in the alloted time: %s", err)	
+	}
 	defer server.Stop()
 
 	//create openshift client
 	osclient := server.OsClient()
 
 	//create kube client
-	kubeclient :=server.KubeClient()
+	//kubeclient :=server.KubeClient()
 	
 	/*
 	//create config via openshift command
 	opts := kubeopts.KubeConfig{
 		Config: "buildcfg/buildcfg.json"
 	}
-	sourceConfig := new(buildapi.BuildConfig)
 	if err := json.Unmarshal(
-	buildConfig, err := client.CreateBuildConfig()
+*/
+	sourceConfig := &buildapi.BuildConfig{}
+	_, err = osclient.CreateBuildConfig(sourceConfig)
 	//validate output matches expectation
 	if err != nil{
-		t.Fail("Create of buildConfig failed with error:", err)
+		t.Error("Create of buildConfig failed with error: %s", err)
 	}
-*/
 
 
   //trigger build
